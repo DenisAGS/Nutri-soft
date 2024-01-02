64a57bd6c775845ff3512d4138b0cd59a230987b
DannaLuisa <danna12.garo@gmail.com>
2024-01-02 17:37:31 -0600

    Validacion que todos los datos de registro e iniciar sesion esten llenos

--- src/app/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/iniciar-sesion/iniciar-sesion.component.ts
@@ -11,26 +11,37 @@ export class IniciarSesionComponent {
   correo: string = '';
   contrasena: string = '';
   showNotification: boolean = false;
+  showInvalidPassword: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   onSubmit() {
     if (!this.correo || !this.contrasena) {
       this.showNotification = true;
+      this.showInvalidPassword = false;
     } else {
-      this.authService
-        .autenticarUsuario(this.correo, this.contrasena)
-        .subscribe(
-          () => {
-            console.log('Sesión iniciada');
-            this.router.navigate(['/inicio']);
-          },
-          (error) => {
-            console.log('Error al iniciar sesión:', error);
-            // Aquí puedes manejar el error de inicio de sesión
-          }
-        );
+      if (this.contrasenaValida()) {
+        this.authService
+          .autenticarUsuario(this.correo, this.contrasena)
+          .subscribe(
+            () => {
+              console.log('Sesión iniciada');
+              this.router.navigate(['/inicio']);
+            },
+            (error) => {
+              console.log('Error al iniciar sesión:', error);
+              // Aquí puedes manejar el error de inicio de sesión
+            }
+          );
         this.showNotification = false;
+        this.showInvalidPassword = false;
+      }
+      else{
+      this.showInvalidPassword = true;
+      }
     }
   }
+  contrasenaValida(): boolean {
+    return true;
+}
 }
